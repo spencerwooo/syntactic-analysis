b2b84269a5af7dbea8f654eb22d1c025b362af0e
spencerwooo <spencerwoo98@gmail.com>
2019-05-22 20:56:14 +0800

    Add useful functions

---
+++ analyse.py
@@ -0,0 +1,18 @@
+"""
+LR(1) 语法分析
+"""
+
+def closure():
+  pass
+
+def goto():
+  pass
+
+def printAnalysisTable():
+  pass
+
+def main():
+  print('Hello')
+
+if __name__ == "__main__":
+    main()
