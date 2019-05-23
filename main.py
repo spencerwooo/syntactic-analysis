8b71a51d1c360abe95ba4cd445959cf61dc9c47b
spencerwooo <spencerwoo98@gmail.com>
2019-05-23 10:01:42 +0800

    Add encoding

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-"""
-LR(1) 语法分析
-"""
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import parserUtils
 
--- parserUtils.py
+++ parserUtils.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 # 从文件中读文法列表
 def readGrammar(filePath):
   grammar = []
